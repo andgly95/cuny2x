b37df93588eacd6c65758f3cee205739e7c8bdc6
Andrew Glyadchenko <andrewgly95@gmail.com>
2018-06-01 10:22:51 -0400

    Created and implemented photo object

--- Day3/OOProject/Photo.js
+++ Day3/OOProject/Photo.js
@@ -1,5 +1,41 @@
+var myAlbum = new Album("My Album");
+
+
+function displayName() {
+    document.getElementById("albumname").innerHTML = myAlbum.name;
+    document.getElementById("addalbum").remove();
+}
+
+function addPhoto() {
+    let name = document.getElementById("photoname").innerText;
+    let location = document.getElementById("photolocation").innerText;
+    console.log("New Photo Added: "+name);
+
+    myAlbum.addPhoto(name, location);
+}
+
 function Photo(name, location){
     this.name = name;
     this.location = location;
 }
-var newPhoto = new Photo("ball","NYC");
+function Album(name){
+    this.name = name;
+    this.photos = [];
+    this.size = 0;
+    console.log("Album Made")
+    this.addPhoto = function(name, location){
+        var newPhoto = new Photo(name, location);
+        this.photos.push(newPhoto);
+        console.log("New Photo Added: "+name);
+        this.size++;
+    }
+    this.displayPhotos = function(){
+        for(let i=0;i<photos.size();i++){
+            console.log("Photo "+photos[i].name);
+        }
+    }
+}
+
+
+
+
