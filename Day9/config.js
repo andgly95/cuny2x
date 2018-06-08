16d0f6f82a3897c70c094be063a015cda04c4d5e
Andrew Glyadchenko <andrewgly95@gmail.com>
2018-06-08 14:59:03 -0400

    Added keys to gitignore

--- Day9/config.js
+++ Day9/config.js
@@ -6,8 +6,8 @@
  * access_token_secret
  */
 module.exports = {
-    consumer_key: 'mERD1NbKJ8YEl3SWjaHqMFv68',  
-    consumer_secret: '0sR9KgzGrATaEsSxQpBQTTUiMJCqbGWTMS0KleR1cOvT8XjuTh',
+    consumer_key: 'eJBRRniQIoA06VTFz9Y99Vzd7',  
+    consumer_secret: 'Vkmtkr2lsZDYTnN4V4sgxebH2M8JZQIJQsIJnJapSv9laj1hXp',
     access_token: '1005150554342002689-ufipNlx377qdOG7Y74XW80mK4AkiHR',  
     access_token_secret: 'ZFzIL2hQpq0PyZdEhIw43wfxY7qVYlNKbDZfocJXaMCwM'
   }
